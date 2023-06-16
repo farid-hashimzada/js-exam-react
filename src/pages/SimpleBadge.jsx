817dcd9c545758c4729698db42bd8616818216e9
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-06-16 11:03:38 -0700

    react-router, API-tasks

--- src/pages/SimpleBadge.jsx
+++ src/pages/SimpleBadge.jsx
@@ -3,7 +3,7 @@ import Badge from '@mui/material/Badge';
 import MailIcon from '@mui/icons-material/Mail';
 
 export default function SimpleBadge() {
-  const [count,setCount] = useState('0')
+  const [count,setCount] = useState('0') // bu 0-ri number kimi goturmurdu.Mende string olaraq verdim
   return (
     <Badge badgeContent={count} color="primary">
       <MailIcon color="action" />
